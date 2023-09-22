a8acc21e7881d0b2bc12bf0046254b319987c0e9
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-09-22 16:57:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 echo "Downloading depency runtime and SDK"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install --system runtime/org.gnome.Platform/x86_64/44 runtime/org.gnome.Sdk/x86_64/44 -y
+flatpak install --system runtime/org.gnome.Platform/x86_64/44 runtime/org.gnome.Sdk/x86_64/44 -y > /dev/null 2>&1
 echo "Building app with Flatpak builder"
-git clone https://github.com/vikdevelop/DesktopLinkCreator /tmp/DesktopLinkCreator
+git clone https://github.com/vikdevelop/DesktopLinkCreator /tmp/DesktopLinkCreator > /dev/null 2>&1
 cd /tmp/DesktopLinkCreator
-flatpak-builder build *.yaml --install --user
+flatpak-builder build *.yaml --install --user > /dev/null 2>&1
 cd
 rm -rf /tmp/DesktopLinkCreator
 echo "DesktopLinkCreator has been installed successfully! Try run the app from desktop or terminal command 'flatpak run io.github.vikdevelop.DesktopLinkCreator'"
